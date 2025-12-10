a7b63f4e76bf8ee55aac61eb0f04476229797649
lucassamel <lucas.samel97@gmail.com>
2025-12-09 21:49:20 -0300

    Padronização nas requisições

--- ApiTemplate.API/Controllers/ProductsController.cs
+++ ApiTemplate.API/Controllers/ProductsController.cs
@@ -1,27 +1,72 @@
 ﻿using ApiTemplate.Application.DTOs;
 using ApiTemplate.Application.Interfaces;
+using ApiTemplate.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTemplate.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class ProductsController(IProductService productService) : ControllerBase
     {
         private readonly IProductService _productService = productService;
 
+        /// <summary>
+        ///     Obtém uma lista de Produtos
+        /// </summary>
+        /// <remarks>
+        ///     Obtém uma lista de Produtos
+        /// </remarks>        
+        /// <returns>Uma lista com todos os produtos</returns>
+        /// <response code="200">Retorna uma lista de produtos.</response>
+        /// <response code="400">Querystring inválida.</response>
+        /// <response code="404">Se nenhum produto for encontrado.</response>
+        /// <response code="500">Erro interno.</response>
         [HttpGet]
+        [ProducesResponseType(typeof(Response<IEnumerable<ProductDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response<IEnumerable<string>>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<string>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetAll()
         {
             var products = await _productService.GetAllProductsAsync();
-            return Ok(products);
+
+            if (products == null || !products.Any())
+            {
+                return NotFound(new Response<string>("Nenhum produto foi encontrado."));
+            }
+
+            return Ok(new Response<IEnumerable<ProductDto?>?>(products));
         }
 
+        /// <summary>
+        ///     Obtém um produto por ID
+        /// </summary>
+        /// <remarks>
+        ///     Obtém um produto por ID
+        /// </remarks>        
+        /// <returns>Um produto</returns>
+        /// <response code="200">Retorna um produto</response>
+        /// <response code="400">Querystring inválida.</response>
+        /// <response code="404">Se nenhum produto for encontrado.</response>
+        /// <response code="500">Erro interno.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Response<ProductDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response<IEnumerable<string>>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response<string>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetById(Guid id)
         {
             var product = await _productService.GetProductByIdAsync(id);
-            return product != null ? Ok(product) : NotFound();
+
+            if (product == null)
+            {
+                return NotFound(new Response<string>("Produto não encontrado."));
+            }
+
+            return Ok(new Response<ProductDto>(product));
         }
 
         [HttpPost]
@@ -38,6 +83,16 @@ namespace ApiTemplate.API.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        ///     Deleta um produto por ID
+        /// </summary>
+        /// <remarks>
+        ///     Deleta um produto por ID
+        /// </remarks>        
+        /// <returns>Um produto</returns>
+        /// <response code="204">Produto deletado</response>        
+        /// <response code="404">Se nenhum produto for encontrado.</response>
+        /// <response code="500">Erro interno.</response>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
---
+++ ApiTemplate.Application/DTOs/Response.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ApiTemplate.Application.DTOs
+{
+    public class Response<TEntity>
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public TEntity? Data { get; protected set; } = default;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public List<string>? Erros { get; private set; } = default;
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+        public bool IsSuccess { get; set; } = true;
+
+        public Response(TEntity data, List<string> erros)
+        {
+            Data = data;
+            Erros = erros;
+        }
+
+        public Response(List<string> erros)
+            => Erros = erros;
+        public Response(TEntity data)
+            => Data = data;
+        public Response(string erro)
+            => Erros = [erro];
+
+        public static SuccessResponse ReturnSucess(string message)
+            => new(message);
+        public class SuccessResponse(string message)
+        {
+            public string Message { get; set; } = message;
+        }
+
+        public static Response<string> NotFound() => new("A consulta não retornou dados.");
+    }
+}
